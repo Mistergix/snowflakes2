dfbb3619cab3357d1c7259b1169028d234d31701
Céline <cceline74@hotmail.fr>
2020-11-18 12:33:51 +0100

    Ajout de la grille et des voisins d'hexagones

---
+++ Assets/Scripts/HexaCell.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TetrisCity.Celine
+{
+	public class HexaCell : MonoBehaviour
+	{
+        private int q, r, s;
+        private float state, oldState;
+        private bool isEdge;
+
+        public void Init(int q, int r, float state, bool isEdge)
+        {
+            this.q = q;
+            this.r = r;
+            this.state = state;
+            this.isEdge = isEdge;
+            oldState = state;
+            s = -q - r;
+        }
+
+        public List<Vector2Int> GetFalseNeightbours()
+        {
+            List<Vector2Int> neightbours = new List<Vector2Int>();
+            List<Vector2Int> offset = new List<Vector2Int>()
+            {
+                new Vector2Int(1,0),
+                new Vector2Int(1,-1),
+                new Vector2Int(0,-1),
+                new Vector2Int(-1,0),
+                new Vector2Int(-1,1),
+                new Vector2Int(0,1)
+            };
+
+            foreach (var o in offset)
+            {
+                neightbours.Add(new Vector2Int(q + o.x, r + o.y));
+            }
+
+            return neightbours;
+        }
+	    // Start is called before the first frame update
+   		void Start()
+   		{
+  		    
+    	}
+
+		// Update is called once per frame
+    	void Update()
+    	{
+        		
+    	}
+	}
+}
---
+++ Assets/Scripts/HexaGrid.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TetrisCity.Celine
+{
+	public class HexaGrid : MonoBehaviour
+	{
+        [SerializeField]
+        private int rayon;
+
+        [SerializeField]
+        private HexaCell hexagon;
+
+        private int nbCellsbyWidth;
+
+        private Grid grid;
+
+        private HashSet<Vector2Int> positions;
+
+        private Dictionary<Vector2Int, HexaCell> cells;
+
+        // Start is called before the first frame update
+   	    void Start()
+   		{
+            cells = new Dictionary<Vector2Int, HexaCell>();
+            grid = GetComponent<Grid>();
+            nbCellsbyWidth = 2 * rayon + 1;
+
+            positions = new HashSet<Vector2Int>();
+            for(int q = -rayon; q <= rayon; q++)
+            {
+                int r1 = Mathf.Max(-rayon, -q - rayon);
+                int r2 = Mathf.Min(rayon, -q + rayon);
+                for(int r = r1; r <= r2; r++)
+                {
+                    positions.Add(new Vector2Int(q, r));
+                }
+            }
+
+            foreach (var x in positions)
+            {
+                HexaCell cell = Instantiate(hexagon);
+                cells.Add(x, cell);
+                cell.Init(x.x, x.y, 0,GetNeightbours(cell).Count!=6);
+                cell.transform.position = grid.CellToWorld(new Vector3Int(x.x, x.y, 0));
+                cell.gameObject.name = $"hexacell {x.x},{x.y},{GetNeightbours(cell).Count != 6}";
+            }
+
+
+    	}
+
+        public List<HexaCell> GetNeightbours(HexaCell cell)
+        {
+            List<Vector2Int> falseNeightbours = cell.GetFalseNeightbours();
+            List<HexaCell> hexacells = new List<HexaCell>();
+
+            foreach(var x in falseNeightbours)
+            {
+                if (cells.TryGetValue(x, out HexaCell c))
+                {
+                    hexacells.Add(c);
+                }
+            }
+
+            return hexacells;
+        }
+
+		// Update is called once per frame
+        void Update()
+    	{
+        	
+    	}
+	}
+}
